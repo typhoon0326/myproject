574ea7299054754977084839eb9bfe25306eb6f5
user <user@es302n30>
2018-09-25 11:50:23 +0800

    initial

---
+++ myproject/src/com/tom/typhoon.java
@@ -0,0 +1,15 @@
+package com.tom;
+
+public class typhoon {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String firstLine = ("Why!");
+		System.out.println(firstLine);
+		System.out.println("The value of firstline is ");
+		System.out.println(FirstLine);
+
+	}
+
+}
