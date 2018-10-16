46b3a1be2b12aad06975fc97ff5abd4edd999584
簡佩璇 <user@es302n12>
2018-10-16 11:59:08 +0800

    10/16

--- myproject/src/com/tom/typhoon.java
+++ myproject/src/com/tom/typhoon.java
@@ -4,12 +4,22 @@ public class typhoon {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		String firstLine = ("Why!");
-		System.out.println(firstLine);
-		System.out.println("The value of firstline is ");
-		System.out.println(firstLine);
-
+		//
+		
+		String s1 = "abc";
+		String s2 = "abc";
+		System.out.println(s1 == s2);
+		
+		String S1 = new String("abc");
+		String S2 = new String("abc");
+		System.out.println(S1 == S2);
+		
+		System.out.println("Jason 95 87");
+		String name = "Jason";
+	    int english = 95;
+		int math = 87;
+		
+		System.out.println(name + "\t" + english + "\t" + math + "\t" +(english+math)/2);
 	}
 
 }
